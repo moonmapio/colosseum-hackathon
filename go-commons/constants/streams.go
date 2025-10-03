6b31339069cedfac88e843378fa30b24d0fd09d2
Helmer Barcos <helmer@barcos.co>
2025-10-04 01:03:47 +0200

    feat: add const for go common pkg

---
+++ go-commons/constants/streams.go
@@ -0,0 +1,9 @@
+package constants
+
+const StreamNotify = "notify"
+
+const StreamSolanaMints = "solanamints"
+const StreamSolanaAccounts = "solanaaccounts"
+
+const SubjectSolanaLogsMintCreate = "solanamints.logs.create"
+const SubjectSolanaAccountUpdated = "solanaaccounts.logs.updated"
