5d158688e7317a73df5dd8c663fc141f3140f0e4
Helmer Barcos <helmer@barcos.co>
2025-10-18 16:22:17 +0200

    feat: move fe main app folder structure

--- frontend/moonmap-main-app/next.config.ts
+++ frontend/moonmap-main-app/next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: 'standalone',
+	// domains: [
+	// 	'cdn.memeapi.io',
+	// 	'coin-images.coingecko.com',
+	// 	'github.com',
+	// 	'avatars.moonmap.io',
+	// 	'ipfs.io',
+	// 	'images.pump.fun',
+	// 	'pbs.twimg.com',
+	// 	'thumbnails.padre.gg',
+	// ],
 	devIndicators: false,
 	images: {
 		path: '/',
